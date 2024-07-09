4219bef95449a839b3c8a79457737ee5ec66426b
Evan Galli <evan_g@orange.fr>
2024-07-09 22:00:31 +0200

    feat(viewer): Translate registry types

--- GeneaGrab/Helpers/EnumerateExtension.cs
+++ GeneaGrab/Helpers/EnumerateExtension.cs
@@ -1,51 +1,21 @@
 ﻿using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Globalization;
-using Avalonia.Data.Converters;
 using Avalonia.Markup.Xaml;
 
 namespace GeneaGrab.Helpers;
 
 public sealed class EnumerateExtension(Type type) : MarkupExtension
 {
-    #region MarkupExtension Members
+    [ConstructorArgument("type")] private Type? Type { get; } = type;
 
     public override object ProvideValue(IServiceProvider serviceProvider)
     {
         if (Type == null) throw new InvalidOperationException(@"The EnumType property is not specified.");
-
         var actualType = Nullable.GetUnderlyingType(Type) ?? Type;
-        TypeConverter typeConverter;
-        ICollection standardValues;
+        if (!actualType.IsEnum) throw new ArgumentException($@"The type '{Type}' has no standard values.");
 
-        if ((typeConverter = TypeDescriptor.GetConverter(actualType)) == null || (standardValues = typeConverter.GetStandardValues(serviceProvider as ITypeDescriptorContext)) == null)
-            throw new ArgumentException($@"The type '{Type}' has no standard values.", "value");
-
-        var items = Type == actualType ? new object[standardValues.Count] : new object[standardValues.Count + 1];
-        var index = 0;
-
-        if (Converter == null)
-            foreach (var standardValue in standardValues)
-                items[index++] = standardValue;
-        else
-        {
-            var culture = ConverterCulture ?? CultureInfo.CurrentCulture;
-            foreach (var standardValue in standardValues) items[index++] = Converter.Convert(standardValue, typeof(object), ConverterParameter, culture);
-            if (Type != actualType) items[index] = Converter.Convert(null, typeof(object), ConverterParameter, culture);
-        }
-
+        var standardValues = actualType.GetEnumValues();
+        var items = Type == actualType ? new object?[standardValues.Length] : new object[standardValues.Length + 1];
+        standardValues.CopyTo(items, 0);
         return items;
     }
-
-    #endregion
-
-    #region Properties
-
-    [ConstructorArgument("type")] private Type? Type { get; set; } = type;
-    public IValueConverter? Converter { get; set; }
-    public CultureInfo? ConverterCulture { get; set; }
-    public object? ConverterParameter { get; set; }
-
-    #endregion
 }
