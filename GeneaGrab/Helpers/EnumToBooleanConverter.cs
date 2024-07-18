cdbed2571ae427805f218b3b6d2d3b53df998616
Evan Galli <evan_g@orange.fr>
2024-07-18 21:03:33 +0200

    refactor: Use an ItemsControl to remove the duplication of the RadioButtons

--- GeneaGrab/Helpers/EnumToBooleanConverter.cs
+++ GeneaGrab/Helpers/EnumToBooleanConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Globalization;
 using Avalonia.Data.Converters;
 
@@ -7,18 +8,23 @@ namespace GeneaGrab.Helpers;
 public class EnumToBooleanConverter : IValueConverter
 {
     public Type EnumType { get; set; } = null!;
+    public object? EnumValue { get; set; }
 
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (parameter is not string enumString) throw new ArgumentException("parameter must be an Enum name!");
-        if (value == null || !Enum.IsDefined(EnumType, value)) throw new ArgumentException("value must be an Enum!");
-        var enumValue = Enum.Parse(EnumType, enumString);
-        return enumValue.Equals(value);
+        if (EnumType == null) throw new ArgumentException("An enum type must be set!");
+        EnumValue ??= Enum.ToObject(EnumType, 0);
+        if (EnumValue == null || !Enum.IsDefined(EnumType, EnumValue)) throw new ArgumentException("value must be a valid Enum object!");
+
+        var enumValue = value switch
+        {
+            string enumString => Enum.TryParse(EnumType, enumString, out var enumObj) ? enumObj : null,
+            Enum enumObj => enumObj,
+            _ => throw new ArgumentException("parameter must be an Enum name or object!")
+        };
+        if (enumValue == null || !Enum.IsDefined(EnumType, enumValue)) throw new InvalidEnumArgumentException();
+        return EnumValue.Equals(enumValue);
     }
 
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (parameter is string enumString) return Enum.Parse(EnumType, enumString);
-        throw new ArgumentException("parameter must be an Enum name!");
-    }
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotSupportedException();
 }
